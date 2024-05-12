e8eb3c7e03d171e23a678caa6e39ae5eae074b1a
Chandan <chandank1848@gmail.com>
2024-05-12 09:36:31 +0530

    Today solved problems

---
+++ DSA/Array/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 7, 2, 9,10, 34, 90, 4, 5};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void bubbleSort(int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length-i; j++) {
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+
+}
