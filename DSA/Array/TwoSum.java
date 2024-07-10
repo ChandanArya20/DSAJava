8e2f4890dd138983c5fbd50fe57ec85789ad3c89
Chandan <chandank1848@gmail.com>
2024-07-10 22:56:43 +0530

    solved two sum problem in optimized way Time :   O(n) Space : O(n)

--- DSA/Array/TwoSum.java
+++ DSA/Array/TwoSum.java
@@ -1,12 +1,14 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
     public static void main(String[] args) {
 
-        int[] arr = {3,3};
+        int[] arr = {3,5,10,2,4,9};
 
-        System.out.println(Arrays.toString(twoSum(arr, 6)));
+        System.out.println(Arrays.toString(twoSum2(arr, 5)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -17,6 +19,22 @@ public class TwoSum {
                     return new int[]{i, j};
                 }
             }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            int complement = target-nums[i];
+
+            if(map.containsKey(complement)){
+                return new int[]{i, map.get(complement)};
+            }
+
+            map.put(nums[i], i);
         }
 
         return new int[]{-1, -1};
